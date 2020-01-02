2e320c800799a8db5f95bd5d5514806d5e008292
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-02 23:29:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,6 +30,10 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  return_hash = {
+    key => value
+  }
+  return return_hash
 end
 
 def read_from_hash(hash, key)
